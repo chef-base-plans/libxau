970d764f6fe8db45db8682995ecbe4b3bb740041
rybons <bonnerrk@gmail.com>
2021-03-18 15:12:58 -0400

    Bumped version 1.0.8 -> 1.0.9

--- plan.sh
+++ plan.sh
@@ -1,14 +1,14 @@
 pkg_name=libxau
 pkg_distname=libXau
 pkg_origin=core
-pkg_version=1.0.8
+pkg_version=1.0.9
 pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="X11 authorization library"
 pkg_upstream_url="https://www.x.org/"
 pkg_license=('MIT')
 pkg_source="https://www.x.org/releases/individual/lib/${pkg_distname}-${pkg_version}.tar.bz2"
-pkg_shasum="fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2"
+pkg_shasum="ccf8cbf0dbf676faa2ea0a6d64bcc3b6746064722b606c8c52917ed00dcb73ec"
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make core/pkg-config core/xproto)
 pkg_include_dirs=(include)
